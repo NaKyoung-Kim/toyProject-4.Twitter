e8e7481315771687eeadf52057e2c09d65777709
a419802 <a419802@users.noreply.github.com>
2021-07-21 13:15:26 +0900

    ~ #2.6

--- src/FBase.js
+++ src/FBase.js
@@ -3,13 +3,22 @@ import 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCanOp0KnFkpJjVCg4sNqFXPqKJLUv0DCs",
-  // apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  authDomain: "twitterclone-99156.firebaseapp.com",
+  projectId: "twitterclone-99156",
+  storageBucket: "twitterclone-99156.appspot.com",
+  messagingSenderId: "163869110090",
+  appId: "1:163869110090:web:8b7644de403762d8cd9bb3"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCanOp0KnFkpJjVCg4sNqFXPqKJLUv0DCs",
+//   // apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSEGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
+// };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,26 +1,35 @@
 // import { debuggerStatement } from '@babel/types';
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
+import Navigation from 'components/Navigation';
+import Profile from '../routes/Profile';
 
 const AppRouter = ({isLoggedIn}) => {
   // const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Router>
+      { isLoggedIn && <Navigation />}
       <Switch>
         { isLoggedIn ? (
           <>
             <Route exact path='/'>
-              <Home>
-              </Home>
+              <Home></Home>
             </Route>
+            <Route exact path='/Profile'>
+              <Profile></Profile>
+            </Route>
+            {/* <Redirect from='*' to='/' /> */}
           </>
         ) : (
-          <Route exact path='/'>
-            <Auth>
-            </Auth>
-          </Route>
+          <>
+            <Route exact path='/'>
+              <Auth>
+              </Auth>
+            </Route>
+            {/* <Redirect from='*' to='/' /> */}
+          </>
         )}
       </Switch>
     </Router>
